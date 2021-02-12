9fc1fe02282b994d7c4ab484aa8a65e0cf193b52
Mellow-Max <72469159+Mellow-Max@users.noreply.github.com>
2021-02-12 18:29:04 -0500

    cleaned up code

--- GAME1011_A1_Brazeau-FeeleyMax_HuynhKenny/Assignement 1/Assignment 1.cpp
+++ GAME1011_A1_Brazeau-FeeleyMax_HuynhKenny/Assignement 1/Assignment 1.cpp
@@ -6,8 +6,7 @@
 using namespace std;
 
 
-
-int main()
+auto main() -> int
 {
 	Survey survey;
 	Person** participant = nullptr;
@@ -29,79 +28,5 @@ int main()
 	else
 	{
 		cout << "Please enter a valid number between 1 and 500" << endl;
-	}
-	
-	
-	
+	}	
 }
-
-
-
-
-
-
-
-
-//int main()
-//{
-//	shared_ptr<Person> pp;
-//	shared_ptr<Student> ps;
-//	shared_ptr<NonGamingStudent> psn;
-//	shared_ptr<GamingStudent> psg;
-//
-//	
-//	Survey* surveyPtr;
-//	int NUM_PEOPLES;
-//	bool isGaming;
-//
-//	cout << "Welcome to the entertainment consumption habits of college students Survey" << endl;
-//	cout << "How many participants would you like to survey" << endl;
-//	cin >> NUM_PEOPLES;
-//
-//	surveyPtr = new Survey[NUM_PEOPLES];
-//	srand(time(0));
-//	for(int i = 0; i< NUM_PEOPLES; i++)
-//	{
-//		//randomly generate participants info
-//		isGaming = rand() % 2;
-//		if(isGaming)
-//		{
-//			surveyPtr[i].participants = new GamingStudent(FirstName[rand() % 9] + " " + LastName[rand() % 9],
-//			                                              rand() % 36 + 16, cName[rand() % 4], pName[rand() % 4],
-//			                                              rand() % 8 + 1, gName[rand() % 6], rand() % 40 + 4);
-//		}
-//		else if(!isGaming)
-//		{
-//			surveyPtr[i].participants = new NonGamingStudent(FirstName[rand() % 9] + " " + LastName[rand() % 9],
-//				rand() % 36 + 16, cName[rand() % 4], pName[rand() % 4],
-//				rand() % 8 + 1, gName[rand() % 5], rand() % 40 + 4);
-//		}
-//		
-//	}
-//
-//	for(int i = 0; i < NUM_PEOPLES; i++)
-//	{
-//		cout << surveyPtr[i].participants->GetName() << endl;
-//		cout << surveyPtr[i].participants->IsGaming() << endl;
-//		cout << surveyPtr[i].participants->GetAge() << endl;
-//		if(surveyPtr[i].participants->IsGaming())
-//		{
-//			cout << static_pointer_cast<GamingStudent> (pp)->GetHours() << endl;
-//		}
-//		else if(!surveyPtr[i].participants->IsGaming())
-//		{
-//			cout << static_pointer_cast<NonGamingStudent> (pp)->GetHours() << endl;
-//		}
-//		
-//	}
-//
-//	const int NUM = 5;
-//	
-//	Person* ptr[NUM] = { new GamingStudent(FirstName[rand() % 9] + " " + LastName[rand() % 9], rand() % 36 + 16, cName[rand() % 4], pName[rand() % 4], rand() % 8 + 1, gName[rand() % 6], rand() % 40 + 4) };
-//
-//	//cout << ptr[0]->GetHours();
-//	
-//	//surveyPtr->Process(&surveyPtr, NUM_PEOPLES);
-//
-//	//processing participants info and displays findings	
-//}
